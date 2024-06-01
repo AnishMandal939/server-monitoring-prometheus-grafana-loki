82fc5ec556cfa86a95547bdc5961d965121db675
AnishMandal939 <mandalktaanees12@gmail.com>
2024-06-01 12:25:41 +0545

    feat - custom metrics for reqResTime taken

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 
 const express = require('express');
+
+// response-time
+const responseTime = require('response-time');
+
 //setup prom-client prometheous
 const client = require('prom-client'); // metric collection
 
@@ -13,6 +17,23 @@ const collectDefaultMetrics = client.collectDefaultMetrics;
 
 // call function
 collectDefaultMetrics({register: client.register});
+
+// creating custom metrics
+const reqResTime = new client.Histogram({
+  name: "https_express_req_res_time",
+  help: "This tells how much time is taken by req and res",
+  labelNames: ['method', 'route', 'status_code'],
+  buckets: [1,50,100,200,400,500,800,1000,2000], // data points
+});
+
+// create middleware and make use of package: npm i response-time
+app.use(responseTime((req, res, time) => {
+    reqResTime.labels({
+    method: req.method, 
+    route: req.url,
+    status_code: req.statusCode
+  }).observe(time)
+}));
 
 app.get("/", (req, res) => {
   return res.json({message: `Hello from express server`});
